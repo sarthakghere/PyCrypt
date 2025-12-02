5909716a0bf8db8d34f20ed384f33db250c2789c
sarthakghere <sg622857@outlook.com>
2025-12-02 16:13:42 +0530

    UI Improved

--- ui/forms.py
+++ ui/forms.py
@@ -4,7 +4,7 @@ class FileEncryptForm(forms.Form):
     file = forms.FileField(label='File to encrypt', required=True)
     publicKey = forms.FileField(label='Receiver\'s Public Key', required=True)
     owner_email = forms.EmailField(label='Your Email', required=True)
-    recipient_email = forms.EmailField(label='Recipient Email (Optional)', required=False, help_text='Optional: Enter email to send the encrypted file.', empty_value=None, )
+    recipient_email = forms.EmailField(label='Recipient Email (Optional)', required=False, help_text='Info: Make sure the file size is less then 25MB.', empty_value=None, )
 
 class FileDecryptForm(forms.Form):
     file = forms.FileField(label='File to decrypt')
